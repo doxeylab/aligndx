06640de1d1adf80c1b6086418eeb8a400a82f280
mshunjan <manjothunjan@gmail.com>
2021-01-28 21:51:07 -0500

    moved .gitignore also added test files to .gitignore

--- experimental/parser.py
+++ experimental/parser.py
@@ -1,4 +1,24 @@
+import numpy as np 
+
 ex_kmer = "GAATCCAATCAGATT"
+def sliding_window(k, seq):
+    '''
+    Scans read using a sliding window of size k and appends them to an array until the last character in the read is reached
+    '''
+    kmer_record = []
+    while k > len(seq):
+        kmer_record.append(seq[:k]) 
+    # Resets k value so we don't have to re-instantiate the class
+    k = k 
+
+def kmer_count(kmer_list): 
+    '''
+    Generates histogram of kmer frequencies for a sequence
+
+    '''
+    kmer_hist = {}
+    for i in np.unique(kmer_list):
+        kmer_hist[i] = kmer_list.count(i)
 
 def fqformat(lines=None):
     format = ["id", 'seq','opt','qual']    
