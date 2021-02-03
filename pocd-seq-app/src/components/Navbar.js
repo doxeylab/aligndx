e6f36838b5871d6fdb79bec34211923150e2005b
Noct0605 <carsonzhang65@gmail.com>
2021-02-03 15:47:45 -0500

    initial

---
+++ pocd-seq-app/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './myStyles.css'
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+
+            )
+        }
+    }
+    
+    export default Navbar;
+    
+    
