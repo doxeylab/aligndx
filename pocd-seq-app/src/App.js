3e6d6cd17ae3f4dbe7a660b893bd32408bb91375
Noct0605 <carsonzhang65@gmail.com>
2021-02-17 16:18:56 -0500

    fixed page nav

--- pocd-seq-app/src/App.js
+++ pocd-seq-app/src/App.js
@@ -13,7 +13,7 @@ import FAQ from './pages/FAQ';
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' component={Home} />
+          <Route path='/' exact component={Home} />
           <Route path='/home' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
