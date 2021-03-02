5bb33c7346633266c0a43e8948eb3c66c035f435
mshunjan <manjothunjan@gmail.com>
2021-03-02 17:43:19 -0500

    enabling multiple uploads to backend

--- pocd-seq-app/api/api.py
+++ pocd-seq-app/api/api.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask, flash, request, redirect, url_for, session, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
+import runsalmon 
  
 ALLOWED_EXTENSIONS = set(['fastq', 'fastq.gz'])
-UPLOAD_FOLDER = 'tmp'
+UPLOAD_FOLDER = './tmp/'
 
 app = Flask(__name__, static_url_path='', static_folder='public', template_folder='public')
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -24,17 +25,24 @@ def allowed_file(filename):
  
 @app.route('/upload', methods=['POST','GET'])
 def fileUpload():
-    target=UPLOAD_FOLDER
-    file = request.files['file']  
-    filename = secure_filename(file.filename)
-    print(filename)
-    if file and allowed_file(filename):
-        destination="/".join([target, filename])
-        file.save(destination)
-        response='success'
-        return response
-    else:  
-        return "Not Valid"
+    # get list of files, if there are more than 1
+    files = request.files.getlist("file")  
+    for file in files: 
+        if file and allowed_file(file.filename): 
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
+    return 'file uploaded successfully' 
+    # target=UPLOAD_FOLDER
+    # file = request.files['file']  
+    # filename = secure_filename(file.filename) 
+    # if file and allowed_file(filename):
+    #     destination="/".join([target, filename])
+    #     file.save(destination)
+    #     print(runsalmon.sanity_check())
+    #     response='success'
+    #     return response
+    # else:  
+    #     return "Not Valid"
 
 @app.route("/files", methods=['GET'])
 def list_files():
