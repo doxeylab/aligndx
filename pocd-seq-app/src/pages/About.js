013f9540153153e06a590baef3248f0a1618f980
Noct0605 <carsonzhang65@gmail.com>
2021-03-22 12:48:46 -0400

    new page styling & code cleanup

--- pocd-seq-app/src/pages/About.js
+++ pocd-seq-app/src/pages/About.js
@@ -4,60 +4,64 @@ import aboutUsImage from '../assets/aboutUsImage.svg';
 import OurGoals from '../assets/OurGoals.svg';
 
 export default function About() {
-  return (
-    <div className="section">
-          <Container className="about-container">
-              {/*About Us section*/}
-              <div className="aboutUs">
-                  <div className="aboutUsText">
-                      <h1>About Us</h1>
-                      <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, maiores sunt quas aut 
-                      doloribus praesentium perspiciatis itaque. Blanditiis non minus ducimus autem hic recusandae 
-                      repellendus amet, reprehenderit, rem debitis eos!
-                      </p>
-                  </div>
-                  <img className="aboutUsImage" src={aboutUsImage} alt='aboutUsImage' />
-              </div>
-
-              {/*Our Goals section*/}
-              <div className="ourGoals" >
-                  <div className="ourGoalsImageWrap">
-                      <img src={OurGoals} alt='OurGoalsImage' />
-                  </div>
-  
-                  <div className="ourGoalsText">
-                      <h1>Our Goals</h1>
-                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere nisi magnam sed quaerat, sint explicabo quis possimus nesciunt nostrum, sapiente enim nam et voluptatibus corrupti illo illum doloribus sit placeat!</p>
-                  </div>
-              </div>
-
-              {/*Company Updates section*/}
-              <div className="companyUpdates">
-                  <div className="companyUpdatesWrapper">
-                      <div className="companyUpdatesTitle">
-                          <h1>Company Updates!</h1>
-                      </div>
-                      <div className="companyUpdatesContent">
-                          <div className="companyUpdatesAnnouncements">
-                              <h2>Announcements</h2>
-                              <p>Lorem ipsum dolor siolorem, odio animi cumque tempore blanditiis necessitatibus minus voluptatum at maxime</p>
-                              {/*eslint-disable-next-line*/}
-                              <a href="javascript:void(0);">Learn More</a>
-                          </div>
-                          <div className="companyUpdatesSocial">
-                              <h2>Follow Us</h2>
-                              <p>Lorem ipsum dolor, coillum iure aliquid? Sunt consequatur debitis ipsum tempore!</p>
-                                <div className="companyUpdatesBox">
-                                    <div className="companyUpdatesIcon"><i class="fab fa-facebook-f"></i></div>
-                                    <div className="companyUpdatesIcon"><i class="fab fa-youtube"></i></div>
-                                    <div className="companyUpdatesIcon"><i class="fab fa-github"></i></div>
-                                </div>
-                          </div>
+    return (
+        <div className="section">
+            <div className="about-container">
+                <Container>
+                    {/*About Us section*/}
+                    <div className="aboutUs">
+                        <div className="aboutUsTitle">
+                            <h1>Who We Are</h1>
                         </div>
-                  </div>
-              </div>
-          </Container>
-    </div>
-  );
+                        <div className="aboutUsBody">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, maiores sunt quas aut
+                                doloribus praesentium perspiciatis itaque. Blanditiis non minus ducimus autem hic recusandae
+                                repellendus amet, reprehenderit, rem debitis eos!
+                            </p>
+                            <img className="aboutUsImage" src={aboutUsImage} alt='aboutUsImage' />
+                        </div>
+                    </div>
+
+                    {/*Our Goals section*/}
+                    <div className="ourGoals" >
+                        <div className="ourGoalsImageWrap">
+                            <img src={OurGoals} alt='OurGoalsImage' />
+                        </div>
+
+                        <div className="ourGoalsText">
+                            <h1>Our Goals</h1>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere nisi magnam sed quaerat, sint explicabo quis possimus nesciunt nostrum, sapiente enim nam et voluptatibus corrupti illo illum doloribus sit placeat!</p>
+                        </div>
+                    </div>
+
+                    {/*Company Updates section*/}
+                    <div className="companyUpdates">
+                        <div className="companyUpdatesWrapper">
+                            <div className="companyUpdatesTitle">
+                                <h1>Company Updates!</h1>
+                            </div>
+                            <div className="companyUpdatesContent">
+                                <div className="companyUpdatesAnnouncements">
+                                    <h2>Announcements</h2>
+                                    <p>Lorem ipsum dolor siolorem, odio animi cumque tempore blanditiis necessitatibus minus voluptatum at maxime</p>
+                                    {/*eslint-disable-next-line*/}
+                                    <a href="javascript:void(0);">Learn More</a>
+                                </div>
+                                <div className="companyUpdatesSocial">
+                                    <h2>Follow Us</h2>
+                                    <p>Lorem ipsum dolor, coillum iure aliquid? Sunt consequatur debitis ipsum tempore!</p>
+                                    <div className="companyUpdatesBox">
+                                        <div className="companyUpdatesIcon"><i class="fab fa-facebook-f"></i></div>
+                                        <div className="companyUpdatesIcon"><i class="fab fa-youtube"></i></div>
+                                        <div className="companyUpdatesIcon"><i class="fab fa-github"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
 }
--- pocd-seq-app/src/pages/FAQ.js
+++ pocd-seq-app/src/pages/FAQ.js
@@ -3,51 +3,55 @@ import Accordion from "../components/Accordion.js"
 import { Container } from 'react-bootstrap';
 
 const FAQ = () => {
-    return ( 
-            <div className="section">
-                <Container className="faq-container">
-                <h1>Frequently Asked Questions</h1>
-                <div className="accordionWrap">
-                    <Accordion 
-                        title="Question 1" 
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    return (
+        <div className="section">
+            <div className="faq-container">
+                <Container>
+                    <div className="faqTitle">
+                        <h1>Frequently Asked Questions</h1>
+                    </div>
+                    <div className="accordionWrap">
+                        <Accordion
+                            title="Question 1"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." 
-                    />
-                    <Accordion 
-                        title="Question 2" 
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                        />
+                        <Accordion
+                            title="Question 2"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." 
-                    />
-                    <Accordion 
-                        title="Question 3" 
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                        />
+                        <Accordion
+                            title="Question 3"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." 
-                    />
-                    <Accordion 
-                        title="Question 4" 
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                        />
+                        <Accordion
+                            title="Question 4"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." 
-                    />
-                    <Accordion 
-                        title="Question 5" 
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                        />
+                        <Accordion
+                            title="Question 5"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." 
-                    />
-                    <Accordion 
-                        title="Question 6" 
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                        />
+                        <Accordion
+                            title="Question 6"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." 
-                    />
-                </div>
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                        />
+                    </div>
                 </Container>
             </div>
-     );
+        </div>
+    );
 }
- 
+
 export default FAQ;
