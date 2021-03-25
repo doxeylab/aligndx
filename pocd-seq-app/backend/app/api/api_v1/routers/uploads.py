d7e5e481d7258fab8edc5f9434f33aa72b3aba48
mshunjan <manjothunjan@gmail.com>
2021-03-24 21:50:21 -0400

    completed migration to FastApi

--- pocd-seq-app/backend/app/api/api_v1/routers/uploads.py
+++ pocd-seq-app/backend/app/api/api_v1/routers/uploads.py
@@ -1,28 +1,29 @@
 from fastapi import APIRouter, File, UploadFile, Form 
 from typing import List
 import shutil, os
-from scripts import fqsplit
+from scripts import fqsplit 
 
-UPLOAD_FOLDER = './uploads' 
+UPLOAD_FOLDER = './uploads'   
 
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
- 
+
 router = APIRouter()
 
-@router.post("/uploadfiles/") 
+@router.post("/") 
 # ... is how pydantic declares a field as required  
 # type hints declared via key: type
 async def create_upload_files(files: List[UploadFile] = File(...), token: str = Form(...)):
     for file in files: 
         # get file name
         sample_name = file.filename.split('.')[0]
-        
+        sample_dir = os.path.join(UPLOAD_FOLDER, sample_name)
         # create directory for uploaded sample, only if it hasn't been uploaded before
         # should I consider allowing repeated uploads of the same file?
         if not os.path.isdir(UPLOAD_FOLDER):
-            os.mkdir(os.path.join(UPLOAD_FOLDER, sample_name))
-        
+            os.mkdir(UPLOAD_FOLDER)
+        if not os.path.isdir(sample_dir):
+            os.mkdir(sample_dir)
         # declare upload location
         file_location = os.path.join(UPLOAD_FOLDER, sample_name, file.filename)
         with open(file_location, "wb+") as file_object:
@@ -31,6 +32,8 @@ async def create_upload_files(files: List[UploadFile] = File(...), token: str = 
         
         # splits uploaded fastq file into evenly distributed chunks
         fqsplit.chunker(file_location) 
+
     # returns the names of files that were uploaded 
-    return {"filenames": [file.filename for file in files]} 
+    return {"filenames": [file.filename for file in files] 
+            } 
  
--- pocd-seq-app/backend/app/main.py
+++ pocd-seq-app/backend/app/main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware 
-import uvicorn
+# import uvicorn
 
 from app.api.api_v1.routers import uploads, results
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "uploads",
+        "description": "Accepts .fastq and .fastq.gz files. The **chunking** logic is also here.",
+    },
+    {
+        "name": "results",
+        "description": "Salmon _quantification_ of chunks.",
+        "externalDocs": {
+            "description": "Salmon docs",
+            "url": "https://salmon.readthedocs.io/en/latest/index.html",
+        },
+    },
+]
+
+app = FastAPI(
+    title = 'AlignDX',
+    description = "This is the restful API for the AlignDX application. Here you will find the auto docs genereated for the API endpoints",
+    version="1.0",
+    openapi_tags=tags_metadata
+)
  
 
 origins = [
@@ -23,8 +43,18 @@ app.add_middleware(
 
 
 # app.include_router(users.router)
-app.include_router(uploads.router) 
-app.include_router(results.router)
+app.include_router(uploads.router,
+    prefix="/uploads",
+    tags=["uploads"],
+    # dependencies=[Depends(get_token_header)],
+    responses={408: {"description": "Ain't gonna work buddy"}},
+) 
+app.include_router(results.router,
+    prefix="/results",
+    tags=["results"],
+    # dependencies=[Depends(get_token_header)],
+    responses={408: {"description": "Ain't gonna work buddy"}},
+)
 
 @app.get("/")
 async def root():
@@ -34,5 +64,5 @@ async def root():
 async def root():
     return {"message": "This is the root endpoint for version 1 of the api"}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
