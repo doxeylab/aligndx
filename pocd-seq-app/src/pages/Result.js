df3053f7d0a808976040b80163904fdceb7283ed
Noct0605 <carsonzhang65@gmail.com>
2021-03-30 16:34:09 -0400

    conflict issue

--- pocd-seq-app/src/pages/Result.js
+++ pocd-seq-app/src/pages/Result.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Green_Check from '../assets/Green_Check.png'
 import Red_X from '../assets/Red_X.png'
@@ -7,9 +7,15 @@ import Barchart from '../components/BarChart.js'
 import { Link } from 'react-router-dom';
 
 const Result = () => {
+    try {
+		var url_id = window.location.href.split('#/?')[1].split('&')[0].slice('id='.length)
+	} catch(err) {
+		var url_id = undefined
+	}
+
     const [data, setData] = useState([{ "index": "TEST1", "column_category": 6 },
     { "index": "TEST2", "column_category": 12 },
-    { "index": "TEST3", "column_category": 3 }])
+    { "index": "TEST3", "column_category": 3 } ])
 
     const [result, setResult] = useState(null);
     const [sample, setSample] = useState(null);
@@ -20,49 +26,49 @@ const Result = () => {
     const [indexes, setIndexes] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:8080/results')
-            .then(res => {
-                console.log(res.data)
-                setResult(res.data.detection_result)
-                setSample(res.data.sample_name)
-                setPathogen(res.data.detected_pathogen)
-                setColumns(res.data.columns)
-                setIndexes(res.data.index)
-                setData(res.data.data)
-            })
-            .catch(() => {
-                console.log('Error')
-            })
+      axios.get('http://localhost:8080/results/' + url_id)
+          .then(res => {
+              console.log(res.data)
+              setResult(res.data.detection_result)
+              setSample(res.data.sample_name)
+              setPathogen(res.data.detected_pathogen)
+              setColumns(res.data.columns)
+              setIndexes(res.data.index)
+              setData(res.data.data)
+          })
+          .catch(() => {
+              console.log('Error')
+          })
     }, [])
 
     return (
         <div className="section">
             <div className="result-container">
                 <Container>
-                    {/* Displays either positive or negative */}
-                    {result ?
-                        <Row className="resultNegative">
-                            <h1 >
-                                <img className="Red_X" src={Red_X} alt='Red_X' />{result} for ({pathogen})
+                {result ?     
+                    <Row className="resultNegative">
+                        <h1 >
+                            <img className="Red_X" src={Red_X} alt='Red_X' />{result} for ({pathogen})
                         </h1>
-                        </Row>
-                        :
-                        <Row className="resultPositive">
-                            <h1 >
-                                <img className="Green_Check" src={Green_Check} alt='Green_Check' />{result} for ({pathogen})
+                    </Row>
+                :
+                    <Row className="resultPositive">
+                        <h1 >
+                            <img className="Green_Check" src={Green_Check} alt='Green_Check' />{result} for ({pathogen})
                         </h1>
-                        </Row>
-                    }
+                    </Row>
+                }
                     <Row className="resultPageBody">
-                        <Col className='barGraph'>
+                        <Col className = 'barGraph'>
                             <Barchart data={data} />
                         </Col>
-                        <Col className='sampleInfo'>
-                            <h1>Sample: {sample}</h1>
+
+                        <Col className = 'sampleInfo'>
+                            <h1>
+                                Sample: {sample}
+                            </h1>
                             <p>
-                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in 
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                                Excepteur sint occaecat cupidatat non proident."
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
                             </p>
                         </Col>
                     </Row>
@@ -74,11 +80,12 @@ const Result = () => {
                                 <button className="resultPageBtn upload-btn">Upload New</button>
                             </Link>
                         </Col>
+
                     </Row>
                 </Container>
             </div>
         </div>
     );
-}
-
+  }
+   
 export default Result;
