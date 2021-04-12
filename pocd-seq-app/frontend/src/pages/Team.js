d9dc20b03eb7d41f228c8b29b76feb78a3805055
Carson Zhang <carsonzhang65@gmail.com>
2021-04-11 22:09:25 -0400

    new team page styling

--- pocd-seq-app/frontend/src/pages/Team.js
+++ pocd-seq-app/frontend/src/pages/Team.js
@@ -9,7 +9,8 @@ export default function Team() {
   return (
     <div className="section">
       <div className="team-container">
-          <div className="teamWrap">
+        <div className="teamWrap">
+          <div className="teamWrapWidth">
             <div className="teamTitle">
               <h1>Meet Our Team</h1>
             </div>
@@ -18,29 +19,37 @@ export default function Team() {
               <div className="professorTitle"><h2>Our Advisors</h2></div>
               <div className="professorWrap">
                 <div className="teamProfileCol">
-                  <a href="https://uwaterloo.ca/biology/people-profiles/andrew-c-doxey">
-                    <div className="teamProfilePictures"><img src={ADoxey} alt='ADoxey' /></div>
-                  </a>
-                  <div className="teamProfileText">
-                    <h2>Andrew Doxey</h2>
-                    <h4>University of Waterloo</h4>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  <div>
+                    <a href="https://uwaterloo.ca/biology/people-profiles/andrew-c-doxey">
+                      <div className="teamProfilePictures"><img src={ADoxey} alt='ADoxey' /></div>
+                    </a>
+                    <div className="teamProfileText">
+                      <div>
+                        <h2>Andrew Doxey</h2>
+                        <h4>University of Waterloo</h4>
+                        <p>
+                          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="teamProfileCol">
-                  <a href="https://healthsci.mcmaster.ca/firh/faculty-member-details/hirota-jeremy">
-                    <div className="teamProfilePictures"><img src={JHirota} alt='JHirota' /></div>
-                  </a>
-                  <div className="teamProfileText">
-                    <h2>Jeremy Hirota</h2>
-                    <h4>McMaster University</h4>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
+                  <div>
+                    <a href="https://healthsci.mcmaster.ca/firh/faculty-member-details/hirota-jeremy">
+                      <div className="teamProfilePictures"><img src={JHirota} alt='JHirota' /></div>
+                    </a>
+                    <div className="teamProfileText">
+                      <div>
+                        <h2>Jeremy Hirota</h2>
+                        <h4>McMaster University</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
+                {/*PlaceHolder Div */}
+                <div className="teamProfileCol"></div>
               </div>
             </div>
             {/*Developers section*/}
@@ -48,44 +57,57 @@ export default function Team() {
               <div className="developerTitle"><h2>Our Developers</h2></div>
               <div className="developerWrap">
                 <div className="teamProfileCol">
-                  <a href="https://www.linkedin.com/in/manjotsinghhunjan/">
-                    <div className="teamProfilePictures"><img src={Manjot} alt='Manjot' /></div>
-                  </a>
-                  <div className="teamProfileText">
-                    <h2>Manjot Hunjun</h2>
-                    <h4>Full Stack Developer</h4>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  <div>
+                    <a href="https://www.linkedin.com/in/manjotsinghhunjan/">
+                      <div className="teamProfilePictures"><img src={Manjot} alt='Manjot' /></div>
+                    </a>
+                    <div className="teamProfileText">
+                      <div>
+                        <h2>Manjot Hunjun</h2>
+                        <h4>Full Stack Developer</h4>
+                        <p>
+                          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="teamProfileCol">
-                  <a href="https://www.linkedin.com/in/william-zhen/">
-                    <div className="teamProfilePictures"><img src={William} alt='William' /></div>
-                  </a>
-                  <div className="teamProfileText">
-                    <h2>William Zhen</h2>
-                    <h4>UI/UX Developer</h4>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  <div>
+                    <a href="https://www.linkedin.com/in/william-zhen/">
+                      <div className="teamProfilePictures"><img src={William} alt='William' /></div>
+                    </a>
+                    <div className="teamProfileText">
+                      <div>
+                        <h2>William Zhen</h2>
+                        <h4>UI/UX Developer</h4>
+                        <p>
+                          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="teamProfileCol">
-                  <a href="https://www.linkedin.com/in/carson-zhang-71a59219b/">
-                    <div className="teamProfilePictures"><img src={Carson} alt='Carson' /></div>
-                  </a>
-                  <div className="teamProfileText">
-                    <h2>Carson Zhang</h2>
-                    <h4>Frontend Developer</h4>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  <div>
+                    <a href="https://www.linkedin.com/in/carson-zhang-71a59219b/">
+                      <div className="teamProfilePictures"><img src={Carson} alt='Carson' /></div>
+                    </a>
+                    <div className="teamProfileText">
+                      <div>
+                        <h2>Carson Zhang</h2>
+                        <h4>Frontend Developer</h4>
+                        <p>
+                          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
