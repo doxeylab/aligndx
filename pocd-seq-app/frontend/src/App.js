84f4255dec9a77213179e38da75e66052e93afc0
Carson Zhang <carsonzhang65@gmail.com>
2021-05-31 11:23:59 -0400

    merging pull request

--- pocd-seq-app/frontend/src/App.js
+++ pocd-seq-app/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Background from './components/Background';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from "./pages/Home";
 import Contact from './pages/Contact';
