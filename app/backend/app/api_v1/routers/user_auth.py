65ecaa994f7cc641a3f8f96c9c6ae386c239caf5
Linda Yang <lindayang0928@hotmail.com>
2021-10-05 18:35:25 -0400

    Change to allow email login instead of username

--- app/backend/app/api_v1/routers/user_auth.py
+++ app/backend/app/api_v1/routers/user_auth.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 from app.db.models import User as UserRepo
-from fastapi import Depends, HTTPException, APIRouter, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -12,7 +12,7 @@ router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-SECRET_KEY = "07d6e91a7e61adcc965da5a176e376c0d0cf36ddce3c85f8e79429243bf13326"
+SECRET_KEY = "74ec68e4e3540e4a6ced9af05d26ea6858bc73a9e3c5cc701228fc37481ad9f7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -21,25 +21,28 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # ---- MODELS ----
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 
 class TokenData(BaseModel):
-    username: Optional[str] = None
+    email: Optional[str] = None
 
 
+# Base model user information the frontend needs
 class User(BaseModel):
-    username: str
     email: str
     name: str
 
 
+# Inherited model - used for when user makes a request to create an account
 class UserTemp(User):
     password: str
 
 
+# Inherited model - used to store the user in the DB
 class UserInDB(User):
     hashed_password: str
 
@@ -55,25 +58,26 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
+        email: str = payload.get("sub")
+        if email is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        token_data = TokenData(email=email)
     except JWTError:
         raise credentials_exception
-    user = await UserRepo.get(token_data.username)
+    user = await UserRepo.get(token_data.email)
     if user is None:
         raise credentials_exception
     return user
 
 
 # Authenticate the user: verify user exists and password is correct
-async def authenticate_user(username: str, password: str):
+async def authenticate_user(email: str, password: str):
     # Verify that the password is the correct password
     def verify_password(plain_password, hashed_password):
         return pwd_context.verify(plain_password, hashed_password)
 
-    user_res = await UserRepo.get(username)
+    user_res = await UserRepo.get(email)
+
     if not user_res:
         return False
 
@@ -95,29 +99,39 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-@router.post("/create_user")
+@router.post("/create_user", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserTemp):
+    user_from_db = await UserRepo.get(user.email)
+
+    if user_from_db:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User already exists, please log in.",
+        )
+
     hashed_password = pwd_context.hash(user.password)
-    db_user = UserInDB(username=user.username, email=user.email, name=user.name,
-                       hashed_password=hashed_password)
+    db_user = UserInDB(
+        email=user.email,
+        name=user.name,
+        hashed_password=hashed_password,
+    )
     await UserRepo.create(db_user)
-    return User(username=db_user.username, email=db_user.email,
-                name=db_user.name)
+    return {"status": status.HTTP_201_CREATED}
 
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(
-        form_data: OAuth2PasswordRequestForm = Depends()):
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    # OAuth2PasswordRequestForm has username and password, username = email in our project
     user = await authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
--- app/frontend/src/components/Authentication/SignUpAuth/SignUpAuth.js
+++ app/frontend/src/components/Authentication/SignUpAuth/SignUpAuth.js
@@ -1,15 +1,16 @@
 // React
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 // Components
-import {Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Button from '../../Button';
 import { CircularProgress } from '@material-ui/core';
 // Actions
-import { request } from "../../../http-common";
+import { loginRequest, signupRequest } from "../../../http-common";
+import { useGlobalContext } from "../../../context-provider";
 // styles
-import { FormContainer, FormInput, FormBtn} from '../StyledForm';
+import { FormContainer, FormInput, FormBtn } from '../StyledForm';
 import '../CustomForm.css';
 // Assets
 import GoogleIcon from "../../../assets/AuthenticationIcons/google-icon.png";
@@ -17,40 +18,27 @@ import FacebookIcon from "../../../assets/AuthenticationIcons/facebook-icon.png"
 
 const SignUpAuth = () => {
     const history = useHistory();
-
+    const context = useGlobalContext();
     const [loading, setLoading] = useState(false)
     const [signUp, setSignUp] = useState({
         name: "",
-        username: "",
         email: "",
         password: ""
     })
 
+    const onChangeName = (e) => {
+        setSignUp({ ...signUp, name: e.target.value })
+    }
+
     const onChangeEmail = (e) => {
-        setSignUp({...signUp, email: e.target.value})
+        setSignUp({ ...signUp, email: e.target.value })
     }
 
     const onChangePassword = (e) => {
-        setSignUp({...signUp, password: e.target.value})
+        setSignUp({ ...signUp, password: e.target.value })
     }
 
-    const onChangeName = (e) => {
-        setSignUp({...signUp, name: e.target.value})
-    }
-
-
-    const onChangeUsername = (e) => {
-        setSignUp({...signUp, username: e.target.value})
-    }
-
-    const signup = (signupRequest) => {
-        return request({
-            url: "http://localhost:8080/create_user",
-            method: "POST",
-            body: JSON.stringify(signupRequest),
-        }, "application/json");
-    }
-
+    // TODO - William: should not allow users to signup if the two password fields do not match
     const handleSignUp = (e) => {
         setLoading(true);
         e.preventDefault();
@@ -59,21 +47,38 @@ const SignUpAuth = () => {
             console.log("MISSING PARAMETER")
         }
 
-        const signupRequest = {
+        const signupParams = {
             name: signUp.name,
-            username: signUp.username,
             email: signUp.email,
             password: signUp.password,
         };
 
-        signup(signupRequest)
-            .then(() => {
-                setLoading(false);
-                history.push('/');
+        signupRequest(signupParams)
+            .then((res) => {
+                if (res.status == 201) {
+                    const loginParams = {
+                        username: signUp.email,
+                        password: signUp.password,
+                    };
+
+                    loginRequest(loginParams)
+                        .then((response) => {
+                            localStorage.setItem("accessToken", response.access_token);
+                            setLoading(false)
+                            history.push("/");
+                            context.loadCurrentUser();
+                        })
+                        .catch((error) => {
+                            setLoading(false);
+                            console.log(error);
+                        });
+                }
+
             })
             .catch((err) => {
                 setLoading(false);
                 console.log(err);
+                // TODO - William: should display these errors on the UI so users know why they cannot signup
             });
     }
 
@@ -83,50 +88,50 @@ const SignUpAuth = () => {
             <FormInput>
                 <Col>
                     <Form.Label>Name</Form.Label>
-                    <Form.Control size="lg" type="name" placeholder="Enter name" onChange={onChangeName}/>
+                    <Form.Control size="lg" type="name" placeholder="Enter name" onChange={onChangeName} />
                 </Col>
             </FormInput>
             <FormInput>
                 <Col>
                     <Form.Label>Email</Form.Label>
-                    <Form.Control size="lg" type="email" placeholder="Enter email" onChange={onChangeEmail}/>
+                    <Form.Control size="lg" type="email" placeholder="Enter email" onChange={onChangeEmail} />
                 </Col>
             </FormInput>
             <FormInput>
                 <Col>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control size="lg" type="password" placeholder="Password" onChange={onChangePassword}/>
+                    <Form.Control size="lg" type="password" placeholder="Password" onChange={onChangePassword} />
                 </Col>
             </FormInput>
             <FormInput>
                 <Col>
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control size="lg" type="password" placeholder="Confirm"/>
+                    <Form.Control size="lg" type="password" placeholder="Confirm" />
                 </Col>
             </FormInput>
 
 
             <FormBtn>
                 <Col md={{ span: 6, offset: 3 }}>
-                    <Button fill onClick={handleSignUp}>{loading ? <CircularProgress size={25}/> : "Sign Up"}</Button>
+                    <Button fill onClick={handleSignUp}>{loading ? <CircularProgress size={25} /> : "Sign Up"}</Button>
                 </Col>
             </FormBtn>
 
             <Row>
-                <Col style={{textAlign: "center"}}>
+                <Col style={{ textAlign: "center" }}>
                     <p>Already have an account? <a href="/login">Login</a></p>
                 </Col>
             </Row>
 
             <Row>
-                <Col style={{textAlign: "center"}}>
+                <Col style={{ textAlign: "center" }}>
                     <p>Or connect with</p>
                 </Col>
             </Row>
             <Row>
-                <Col style={{display: 'flex', justifyContent: "center"}}>
-                    <img src={GoogleIcon} alt='google-icon' width="25" height="25" style={{margin: "10px"}}/>
-                    <img src={FacebookIcon} alt='facebook-icon' width="25" height="25" style={{margin: "10px"}}/>
+                <Col style={{ display: 'flex', justifyContent: "center" }}>
+                    <img src={GoogleIcon} alt='google-icon' width="25" height="25" style={{ margin: "10px" }} />
+                    <img src={FacebookIcon} alt='facebook-icon' width="25" height="25" style={{ margin: "10px" }} />
                 </Col>
             </Row>
         </FormContainer>
