bb98f1e80c79e8aa8a6e5e65e23753bb3155f2e6
manjot <mshunjan@github.com>
2021-11-18 23:41:29 +0000

    added setup configurations

--- app/scripts/m1_mac_build.sh
+++ app/scripts/m1_mac_build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash 
 
 # Build and run containers
-docker-compose -f docker-compose.yml up -d --build
+docker-compose -f docker-compose.m1.yml up -d --build
 
 # Hack to wait for postgres container to be up before running alembic migrations
 sleep 5;
 
 # Run migrations
-docker-compose -f docker-compose.yml run --rm -w "/app/app" backend alembic upgrade head
+docker-compose -f docker-compose.m1.yml run --rm -w "/app/app" backend alembic upgrade head
 
 #Just to be safe
 sleep 5;
 
 # Download default index 
-docker-compose -f docker-compose.yml run --rm -w "/app" backend gdown "https://drive.google.com/drive/folders/1G4fJGf_CutQIycQvGTTbSG4IP3LnelKs" -O ./indexes/sars_with_human_decoys/ --folder
+docker-compose -f docker-compose.m1.yml run --rm -w "/app" backend gdown "https://drive.google.com/drive/folders/1G4fJGf_CutQIycQvGTTbSG4IP3LnelKs" -O ./indexes/sars_with_human_decoys/ --folder
 
 # clear screen and list docker container links
 address='http://localhost:' 
