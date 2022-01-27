8e6d90a146d7ce31c983ddcb538667f8517802f4
manjot <mshunjan@github.com>
2022-01-27 21:11:39 +0000

    deleted unnecessary liveresults page

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -13,8 +13,7 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Result from './pages/Result';
 import Signup from './pages/Signup';
-import Team from './pages/Team';
-import LiveResults from './pages/LiveResults';
+import Team from './pages/Team'; 
 import RealTime from './pages/RealTime';
 import GlobalStyle from './StyledGlobal';
 
@@ -48,8 +47,7 @@ function App() {
                                     <Route path='/results' component={Result} />
                                     <Route path='/signup' component={Signup} />
                                     <Route path='/login' component={Login} />
-                                    <Route path='/USER_ID' component={Profile} />
-                                    <Route path='/liveresults' component={LiveResults} />
+                                    <Route path='/USER_ID' component={Profile} /> 
                                     <Route path='/realtime' component={RealTime} />
                                 </Switch>
                                 <Footer />
