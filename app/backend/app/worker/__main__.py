641ddd4535c9aa522522e77d4ec0a22a8aa4122b
Manjot <manjothunjan@gmail.com>
2022-03-16 22:13:03 -0400

    deleted faust worker src code and dependencies

--- app/backend/app/worker/__main__.py
+++
@@ -1,7 +0,0 @@
-import app.worker as worker
-
-worker.set_faust_app_for_worker()
-
-faust_app = worker.get_faust_app()
-
-faust_app.main()
--- app/backend/app/worker/tables/chunk_table.py
+++
@@ -1,9 +0,0 @@
-from app.worker import get_faust_app
-
-faust_app = get_faust_app()
-
-chunk_table = faust_app.GlobalTable(
-    "chunks",
-    default=dict
-)
-  
