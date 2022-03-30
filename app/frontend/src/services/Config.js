10f20195070b38969f33fa64f3c448b3dd59d744
Manjot <manjothunjan@gmail.com>
2022-03-30 19:10:49 -0400

    added string replacement for socket url

--- app/frontend/src/services/Config.js
+++ app/frontend/src/services/Config.js
@@ -26,7 +26,11 @@ export const STANDARD_SUBMISSIONS_URL = USERS + "submissions"
 export const INCOMPLETE_URL = USERS + "incomplete"
 export const LINKED_RESULTS = USERS + "linked_results/"
 
-export const WEBSOCKET_URL = "ws://aligndx.uwaterloo.ca:8080/livegraphs"
+const base_ws_url = "ws"
+const socket_url = URL + "livegraphs"
+export const WEBSOCKET_URL = socket_url.replace(/http/, base_ws_url)
+console.log(WEBSOCKET_URL)
+// export const WEBSOCKET_URL = "ws://aligndx.uwaterloo.ca:8080/livegraphs"
 // export const WEBSOCKET_URL = "ws://localhost:8080/livegraphs"
 
 // metadata
