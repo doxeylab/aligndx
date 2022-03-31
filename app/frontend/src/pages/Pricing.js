00b8f2ad9acbef5f59245c29922ccf02afb67618
Azabelle <azabelle.tale@gmail.com>
2022-03-31 08:55:08 -0400

    Updated Pricing.js

--- app/frontend/src/pages/Pricing.js
+++ app/frontend/src/pages/Pricing.js
@@ -2,21 +2,34 @@ import { Button, Container, Card } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { SendCheck, CheckCircleFill } from "react-bootstrap-icons";
-import { Section } from '../components/Common/PageElement';
+import { Section } from "../components/Common/PageElement";
+
+
 const Pricing = () => {
+  const styles = {
+    button: {
+      fontSize:"22px",
+      borderRadius:"10px"
+    }
+  }
   return (
     <>
-        <Section center id="login">
+      <Section center id="pricing">
         <Container>
           <div className="py-5">
             <Row>
-              <Col className="text-center">
+              <Col className="text-center mb-5">
                 <h2>Analyze your data for free up to 1GB</h2>
               </Col>
             </Row>
             <Row>
-              <Col>
-                <Card className="bg-white p-5 rounded-lg shadow">
+              <Col
+                sm={{ span: 10, offset: 1 }}
+                md={{ span: 8, offset: 2 }}
+                lg={{ span: 4, offset: 0 }}
+                className="mb-5"
+              >
+                <Card className="p-5">
                   <Card.Header className="text-center">
                     <SendCheck color="DodgerBlue" size={96} />
                     <h1 className="font-weight-bold mb-4">Basic</h1>
@@ -24,16 +37,16 @@ const Pricing = () => {
                   <Card.Body className="text-center">
                     <div>
                       <span className="h1 font-weight-bold">$100</span>
-                      <span className="h3 font-weight-normal ml-2">
+                      <span className="h3">
                         / month
                       </span>
                     </div>
-                    <div className="custom-separator my-4 mx-auto bg-primary"></div>
-                    <ul className="list-unstyled my-5 text-small text-left font-weight-normal">
+                    <ul className="list-unstyled my-5 text-left">
                       <li className="mb-3">
                         <CheckCircleFill color="green" size={20} />
-                        <span className="h4 font-weight-bold">&nbsp;Data:</span>
-                        Up to 50 GB
+                        <span className="h4 font-weight-bold">
+                          &nbsp;Up to 100GB
+                        </span>
                       </li>
                       <li className="mb-3">
                         <CheckCircleFill color="green" size={20} />
@@ -44,31 +57,35 @@ const Pricing = () => {
                     </ul>
                   </Card.Body>
                   <Card.Footer className="text-center">
-                    <Button variant="primary" className="shadow rounded-pill">
-                      Join Now
+                    <Button variant="primary" style={styles.button}>
+                      Join now
                     </Button>
                   </Card.Footer>
                 </Card>
               </Col>
-              <Col>
-                <Card className="bg-white p-5 rounded-lg shadow">
+              <Col
+                sm={{ span: 10, offset: 1 }}
+                md={{ span: 8, offset: 2 }}
+                lg={{ span: 4, offset: 0 }}
+              >
+                <Card className="p-5">
                   <Card.Header className="text-center">
                     <SendCheck color="DodgerBlue" size={96} />
                     <h1 className="font-weight-bold mb-4">Premium</h1>
                   </Card.Header>
                   <Card.Body className="text-center">
                     <div>
-                      <span className="h1 font-weight-bold">$1000</span>
-                      <span className="h3 font-weight-normal ml-2">
+                      <span className="h1 font-weight-bold">$150</span>
+                      <span className="h3">
                         / month
                       </span>
                     </div>
-                    <div className="custom-separator my-4 mx-auto bg-primary"></div>
-                    <ul className="list-unstyled my-5 text-small text-left font-weight-normal">
+                    <ul className="list-unstyled my-5 text-left">
                       <li className="mb-3">
                         <CheckCircleFill color="green" size={20} />
-                        <span className="h4 font-weight-bold">&nbsp;Data:</span>
-                        Up to 500 GB
+                        <span className="h4 font-weight-bold">
+                          &nbsp;Up to 500GB
+                        </span>
                       </li>
                       <li className="mb-3">
                         <CheckCircleFill color="green" size={20} />
@@ -79,29 +96,32 @@ const Pricing = () => {
                     </ul>
                   </Card.Body>
                   <Card.Footer className="text-center">
-                    <Button variant="primary" className="shadow rounded-pill">
-                      Join Now
+                    <Button variant="primary" style={styles.button}>
+                      Join now
                     </Button>
                   </Card.Footer>
                 </Card>
               </Col>
-              <Col>
-                <Card className="bg-white p-5 rounded-lg shadow">
+              <Col
+                sm={{ span: 10, offset: 1 }}
+                md={{ span: 8, offset: 2 }}
+                lg={{ span: 4, offset: 0 }}
+              >
+                <Card className="p-5">
                   <Card.Header className="text-center">
                     <SendCheck color="DodgerBlue" size={96} />
-                    <h1 className="font-weight-bold mb-4">Custom</h1>
+                    <h1 className="font-weight-bold mb-4">Custom Quote</h1>
                   </Card.Header>
                   <Card.Body className="text-center">
                     <div>
                       <span className="h1 font-weight-bold">Contact Us</span>
-        
                     </div>
-                    <div className="custom-separator my-4 mx-auto bg-primary"></div>
-                    <ul className="list-unstyled my-5 text-small text-left font-weight-normal">
+                    <ul className="list-unstyled my-5 text-left">
                       <li className="mb-3">
                         <CheckCircleFill color="green" size={20} />
-                        <span className="h4 font-weight-bold">&nbsp;Data:</span>
-                        Up to 50 GB
+                        <span className="h4 font-weight-bold">
+                          &nbsp;Unlimited Data
+                        </span>
                       </li>
                       <li className="mb-3">
                         <CheckCircleFill color="green" size={20} />
@@ -112,7 +132,7 @@ const Pricing = () => {
                     </ul>
                   </Card.Body>
                   <Card.Footer className="text-center">
-                    <Button variant="primary" className="shadow rounded-pill">
+                    <Button variant="primary" style={styles.button}>
                       Get a Quote
                     </Button>
                   </Card.Footer>
@@ -121,8 +141,8 @@ const Pricing = () => {
             </Row>
           </div>
         </Container>
-        </Section>
-      </>
+      </Section>
+    </>
   );
 };
 export default Pricing;
