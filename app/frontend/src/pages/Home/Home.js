133a07d9a8920777970804545984428eecfc95a6
Manjot <manjothunjan@gmail.com>
2022-04-11 16:11:09 -0400

    deleted tutorial component

--- app/frontend/src/pages/Home/Home.js
+++ app/frontend/src/pages/Home/Home.js
@@ -1,13 +1,11 @@
 // React
 import React from 'react';
 import Hero from './Hero';
-import Tutorial from './Tutorial'; 
 
 const Home = (props) => {
     return (
         <>
             <Hero changeProgress={props.changeProgress}/>
-            <Tutorial/>
         </>
     );
 }
