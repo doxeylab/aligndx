b9f95f2bbc62f95db959c2db6eb97e2ffe6578f7
Manjot <manjothunjan@gmail.com>
2022-04-14 13:11:44 -0400

    added minor fix to tool container to send error status on tool error

--- app/tool/app/main.py
+++ app/tool/app/main.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import subprocess 
 import asyncio 
@@ -32,14 +32,10 @@ async def run(commands_lst : list):
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE)
 
-    stdout, stderr = await proc.communicate() 
-    if stdout:
-        print("stdout:")
-        print(stdout.decode())
-        return stdout.decode()
-    if stderr:
-        print(f"commands were {commands}")
-        print("stderr:")
-        print(stderr.decode())
-        return stderr.decode()
+    stdout, stderr = await proc.communicate()  
+    
+    if proc.returncode != 0:
+        raise HTTPException(status_code=400, detail=stderr.decode())
+    else:
+        return stdout
         
