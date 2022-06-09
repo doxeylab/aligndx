3570b3e150d887408d52f1b0a21523e95a855fba
manjot <mshunjan@github.com>
2022-06-09 16:09:31 +0000

    fixes for menu and connection pool closing

--- app/backend/app/db/session.py
+++ app/backend/app/db/session.py
@@ -7,5 +7,5 @@ from app.config.settings import settings
 
 # Enabled SQL Alchemy 2.0 API with future flag
 # Enabled logging of engine generated SQL with echo flag
-engine = create_async_engine(settings.async_database_url, future=True, echo=settings.DB_LOGS)
+engine = create_async_engine(settings.async_database_url, future=True, echo=settings.DB_LOGS, pool_size=32, max_overflow=64)
 async_session = sessionmaker(engine, expire_on_commit=False,class_=AsyncSession)
