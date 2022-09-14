46054d66042f8248fc87d9b09478c2c3130b306a
manjot <mshunjan@github.com>
2022-09-14 22:24:04 +0000

    finished simple pipeline in prefect

--- app/backend/app/flows/main.py
+++ app/backend/app/flows/main.py
@@ -1,33 +1,43 @@
 from prefect import flow, task
+import os, shutil
+from pydantic import BaseModel
 
-@task(name="Generate metadata")
-def create_metadata():
-    """
-    Create metadata for a new file upload. Called on the creation of an upload.
-    *Note: To be stored in redis/postgres in the future ...
-    :param file_dir: Complete path for the directory to store file data in.
-    :param filename: The filename of the uploaded file.
-    :param upload_chunk_size: Size (in kilobytes) of each chunk uploaded from the frontend.
-    :param analysis_chunk_size: Size (in kilobytes) of each reassembled chunk to be analyzed by the tool.
-    :param num_upload_chunks: Total number of upload chunks the file is divided into.
-    :param email: Email of the user to notify on file completion.
-    :param fileId: ID of the submission. Must be unique identifier.
-    :param panel: Pathogen panel.
-    :param process: Process for the analysis tool.
-    """    
-    pass
+from app.redis.functions import Handler
+from app.flows.analysis import analysis_pipeline
 
-@flow
-def chunking(
-    name="Real Time Pipeline",
-    descripton="Chunking pipeline for file assembly"
-    ):
-    pass
+class MetaModel(BaseModel):
+    updir: str
+    rdir: str
+    tooldir: str
+    fname: str
+    total: int
+    processed: int 
 
-@flow
-def main(
-    name="Main Pipeline",
-    descripton="Overall flow"
-        ):
-    print('testing')
-    return 42
+@task(name="Update metadata")
+def update_metadata(fileId : str, metadata : MetaModel.dict()):
+    Handler.create(fileId, metadata)
+
+@task(name="Retrive metadata")
+def retrieve(fileId : str):
+    return Handler.retrieve(fileId)
+
+@task(name="Assemble chunks")
+def assemble_chunks(updir,tooldir, total, filename):
+    os.system(f"cat {updir}/{{0..}}{total}* > {tooldir}/{filename}") 
+
+@task(name="Cleanup")
+def cleanup(fdir):
+    shutil.rmtree(fdir)
+
+@flow(name="Chunking Pipeline",
+    descripton="Chunking pipeline for file assembly")
+def pipeline(fileId : str):
+    metadata = retrieve(fileId)
+    
+    if metadata['processed'] == metadata['total']:
+        assemble_chunks(metadata['updir'], metadata['tooldir'], metadata['total'], metadata['fname'])
+        cleanup(metadata['updir'])
+        analysis_pipeline(metadata['tooldir'], metadata['rdir'])
+    else:
+        metadata['processed'] += 1
+        update_metadata(metadata)
