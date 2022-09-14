642b540a272f622e685267c3cb6ca2536999e3a1
manjot <mshunjan@github.com>
2022-09-14 05:45:19 +0000

    added redis tasks

--- app/backend/app/redis/base.py
+++ app/backend/app/redis/base.py
@@ -2,9 +2,3 @@ import redis
 from app.config.settings import settings
 
 r = redis.Redis.from_url(url=settings.REDIS_URL)
-
-print(r)
-r.set('foo', 'bar')
-value = r.get('foo')
-print(value)
-print("testing")
---
+++ app/backend/app/redis/tasks.py
@@ -0,0 +1,9 @@
+from app.redis.base import r  
+
+# task 1 -> create metadata
+def create(id: str, metadata: dict):
+        r.set(id, metadata)
+
+# task 2 -> get metadata
+def retrieve(id: str):
+        r.get(id)
