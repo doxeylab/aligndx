fd528a19a8d18b6e9cbb7e3946d70b0412f2c6a3
manjot <mshunjan@github.com>
2022-11-02 05:10:16 +0000

    Added linting helpers

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+console.log('foo')
+var a;
 export default function Home() {
   return (
     <div className={styles.container}>
