c92adca498b385a1f4f56230a641fa089adc4149
mshunjan <manjothunjan@gmail.com>
2022-11-19 19:02:08 -0500

    updated Uploader for client side usage

--- frontend/src/components/Uploader/Uploader.tsx
+++ frontend/src/components/Uploader/Uploader.tsx
@@ -12,11 +12,12 @@ import Dashboard from "@uppy/react/lib/Dashboard";
 import "@uppy/core/dist/style.css";
 import "@uppy/dashboard/dist/style.css";
 import '@uppy/image-editor/dist/style.css'
+import { useEffect, useState } from "react";
 
 
 interface UploaderProps {
     plugins?: string[];
-    [dashProps:string] :any;
+    [dashProps: string]: any;
 }
 
 const COMPANION_URL = "http://companion.uppy.io";
@@ -24,39 +25,48 @@ const TUS_ENDPOINT = "https://tusd.tusdemo.net/files/";
 
 export default function Uploader(props: UploaderProps) {
     const { plugins, ...dashProps } = props
+    const [uppy, setUppy] = useState(null) // note that `uppy` will be `null` on the server-side in this case
 
-    const uppy = new Uppy({
-        id: "uppy1",
-        autoProceed: false,
-        debug: true
-    })
-        .use(Tus, { endpoint: TUS_ENDPOINT })
-        .use(WebCam, {
-            id: 'MyWebCam',
-            modes: ['picture'],
-            mobileNativeCamera: true
+    useEffect(() => {
+        const uppy = new Uppy({
+            id: "uppy",
+            autoProceed: false,
+            debug: true
         })
-        .use(ImageEditor, {
-            id: "MyImageEditor",
-            quality: 0.8,
-        })
-        .use(GoogleDrive, { companionUrl: COMPANION_URL })
-        .use(DropBox, { companionUrl: COMPANION_URL })
-        .use(OneDrive, { companionUrl: COMPANION_URL })
-        .use(Url, { companionUrl: COMPANION_URL });
+            .use(Tus, { endpoint: TUS_ENDPOINT })
+            .use(WebCam, {
+                id: 'MyWebCam',
+                modes: ['picture'],
+                mobileNativeCamera: true
+            })
+            .use(ImageEditor, {
+                id: "MyImageEditor",
+                quality: 0.8,
+            })
+            .use(GoogleDrive, { companionUrl: COMPANION_URL })
+            .use(DropBox, { companionUrl: COMPANION_URL })
+            .use(OneDrive, { companionUrl: COMPANION_URL })
+            .use(Url, { companionUrl: COMPANION_URL });
+        setUppy(uppy)
+    }, [])
 
     return (
-        <Dashboard
-            uppy={uppy}
-            plugins={
-                plugins ?
-                    plugins
-                    :
-                    ["GoogleDrive", "MyWebCam", "OneDrive", "Dropbox", "Url", "MyImageEditor"]
-            }
-            {...dashProps}
-            proudlyDisplayPoweredByUppy={false}
-            metaFields={[{ id: "name", name: "Name", placeholder: "File name" }]}
-        />
+        <>
+            {uppy ?
+                <Dashboard
+                    uppy={uppy}
+                    plugins={
+                        plugins ?
+                            plugins
+                            :
+                            ["GoogleDrive", "MyWebCam", "OneDrive", "Dropbox", "Url", "MyImageEditor"]
+                    }
+                    {...dashProps}
+                    proudlyDisplayPoweredByUppy={false}
+                    metaFields={[{ id: "name", name: "Name", placeholder: "File name" }]}
+                />
+                :
+                null}
+        </>
     );
 }
