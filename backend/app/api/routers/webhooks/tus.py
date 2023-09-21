6f686d0aa665b098c0e7634b1a6ec13ccc153b8e
mshunjan <manjothunjan@gmail.com>
2023-09-21 02:56:32 -0400

    Fix missing celery task arguments on webhook

--- backend/app/api/routers/webhooks/tus.py
+++ backend/app/api/routers/webhooks/tus.py
@@ -8,32 +8,35 @@ from app.services.auth import get_current_user
 from app.models import submissions, auth
 from app.models.pipelines import inputs
 
-from app.services.db import get_db 
-from app.tasks import retrieve, update_metadata 
+from app.services.db import get_db
+from app.tasks import retrieve, update_metadata
 
-router = APIRouter() 
+router = APIRouter()
+
 
 @router.post("/")
-async def tusd(  
+async def tusd(
     request: Request,
     current_user: auth.UserDTO = Depends(get_current_user),
-    db: AsyncSession = Depends(get_db)
-):  
-    hook_name = request.headers.get('hook-name') 
+    db: AsyncSession = Depends(get_db),
+):
+    hook_name = request.headers.get("hook-name")
     request_body = await request.json()
 
-    tus_data = request_body.get('Upload', {})
-    tus_meta = tus_data.get('MetaData')
-    sub_id, fname, input_id = *(tus_meta.get(i) for i in ['sub_id', 'filename', 'input_id']),
+    tus_data = request_body.get("Upload", {})
+    tus_meta = tus_data.get("MetaData")
+    sub_id, fname, input_id = (
+        *(tus_meta.get(i) for i in ["sub_id", "filename", "input_id"]),
+    )
 
     # Check to see if submission Id exists on metadata
     if sub_id == None or input_id == None:
         raise HTTPException(status_code=405, detail="Missing required metadata")
     metadata = retrieve.s(sub_id)()
 
-    if hook_name == 'post-finish':
+    if hook_name == "post-finish":
         # Enqueue the Celery task to update metadata asynchronously
-        file_id = tus_data.get('ID')
-        update_metadata.apply_async(args=[sub_id])
+        file_id = tus_data.get("ID")
+        update_metadata.apply_async(args=[sub_id, metadata])
 
     return 200
