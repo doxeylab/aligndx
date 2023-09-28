1b3136715e11cbb805884de2607a809821726e4d
mshunjan <manjothunjan@gmail.com>
2023-09-28 17:59:05 -0400

    Change workflows endpoint

--- frontend/src/api/Meta/index.js
+++ frontend/src/api/Meta/index.js
@@ -1,13 +1,11 @@
 import useAxios from '../useAxios'
 
-const meta_endpoint = "metadata"
+const meta_endpoint = 'workflows'
 
 export const useMeta = () => {
-    const { get, post, put, destroy } = useAxios();
+    const { get, post, put, destroy } = useAxios()
 
     return {
-        get_pipelines: () =>
-            get(`${meta_endpoint}/pipelines`),
+        get_pipelines: () => get(`${meta_endpoint}/`),
     }
-
 }
