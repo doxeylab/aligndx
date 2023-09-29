7fd8e262ea4963b3aef6221907b684bc689e593c
mshunjan <manjothunjan@gmail.com>
2023-09-29 19:24:35 -0400

    New json focused redis functions

--- backend/app/tasks/redis/functions.py
+++ backend/app/tasks/redis/functions.py
@@ -1,20 +1,55 @@
-from .base import r  
-import json
- 
-class Handler:
-        # create key,data entry
-        def create(id: str, data: dict):
-                data_json = json.dumps(data)
-                r.set(id, data_json)
-        
-        # get data
-        def retrieve(id: str):
-                data = r.get(id)
-                if data!=None:
-                        return json.loads(data)
-                else: 
-                        return data
+from .base import r
+import json, time
+from typing import Optional
 
-        # delete key
-        def delete(id: str):
-                r.delete(id)
+class Handler:
+    QUEUE_KEY = "job_queue"
+
+    @classmethod
+    def enqueue_job(cls, id: str, data_json: str):
+        r.set(id, data_json) 
+
+        score = time.time()  
+        r.zadd(cls.QUEUE_KEY, {id: score}) 
+
+    @classmethod
+    def dequeue_job(cls):
+        jobs = r.zrange(cls.QUEUE_KEY, 0, 0, withscores=True)
+        if not jobs:
+            return None
+
+        id, _ = jobs[0]
+        r.zrem(cls.QUEUE_KEY, id)
+
+        data_json = r.get(id)
+        if data_json is None:
+            return None
+
+        try:
+            data = json.loads(data_json)
+        except json.JSONDecodeError:
+            return None
+        
+        r.delete(id)
+        
+        return id, data
+
+    @classmethod
+    def get_job_position(cls, id: str) -> Optional[int]:
+        rank = r.zrank(cls.QUEUE_KEY, id)
+        if rank is None:
+            return None  # Job is not in the queue
+        return rank + 1
+
+    @classmethod
+    def create(cls, key: str, data_json: str):
+        r.set(key, data_json)
+
+    @classmethod
+    def retrieve(cls, key: str) -> dict:
+        data_json = r.get(key)
+        return json.loads(data_json) if data_json else None 
+
+    @classmethod
+    def delete(cls, key: str):
+        r.delete(key)
