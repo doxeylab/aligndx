993ef8bdb452becdf8f1f61314eaf571c64d26bd
mshunjan <manjothunjan@gmail.com>
2023-10-05 12:58:27 -0400

    Remove post-terminate web-hook

--- backend/app/api/routers/webhooks/tus.py
+++ backend/app/api/routers/webhooks/tus.py
@@ -1,5 +1,3 @@
-from app.storages.storage_manager import StorageManager
-from app.models.stores import BaseStores
 from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.responses import JSONResponse
 from app.celery.tasks import move_data
@@ -13,13 +11,11 @@ async def tusd(
 ):
     request_body = await request.json()
     hook_name = request_body.get("Type", "") 
-    print(hook_name)
     event = request_body.get("Event",{})
     upload_info = event.get("Upload",{})
 
     metadata = upload_info.get("MetaData")
     sub_id, fname = (*(metadata.get(i) for i in ["sub_id", "filename"]),)
-    storage = StorageManager ()
     if sub_id is None:
         raise HTTPException(
             status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
@@ -29,14 +25,6 @@ async def tusd(
     if hook_name == "post-finish":
         file_id = upload_info.get("ID")
         move_data.delay(sub_id, file_id, fname)
-
-    
-    if hook_name == "post-terminate":
-        file_id = upload_info.get("ID")
-        storage.delete(BaseStores.UPLOADS, file_id)
-        storage.delete(BaseStores.UPLOADS, f"{file_id}.info")
-        storage.delete(BaseStores.UPLOADS, f"{file_id}.lock")
-
 
     return JSONResponse(content={"detail": "Success"}, status_code=200)
 
