d53662a8d6ff63ba3444ea7f21f5a5f6b10f574c
mshunjan <47159122+mshunjan@users.noreply.github.com>
2023-10-06 01:22:53 -0400

    Bug patch file selector (#227)
    * Fix type errors
    
    * Revert "Fix type errors"
    
    This reverts commit 3f7b780cb5fd0d91855b6d605b6271e22051d623.
    
    * Fix type error

--- frontend/src/components/PipelineForm/DynamicInputs/FileSelectorField.tsx
+++ frontend/src/components/PipelineForm/DynamicInputs/FileSelectorField.tsx
@@ -5,101 +5,101 @@ import FormHelperText from '@mui/material/FormHelperText';
 import Box from '@mui/material/Box';
 
 interface FileSelectorProps {
-  name: string;
-  uploader: any;  
-  plugins: any[];
-  readyStatus?: { [key: string]: boolean | null };
-  setReadyStatus?: React.Dispatch<React.SetStateAction<{ [key: string]: boolean | null }>>;
-  [key: string]: any;
+    name: string;
+    uploader: any;
+    plugins: any[];
+    readyStatus?: { [key: string]: boolean | null };
+    setReadyStatus?: React.Dispatch<React.SetStateAction<{ [key: string]: boolean | null }>>;
+    [key: string]: any;
 }
 
 const FileSelectorField: React.FC<FileSelectorProps> = ({
-  name,
-  uploader,
-  plugins,
-  readyStatus,
-  setReadyStatus,
-  ...fileSelectorProps
+    name,
+    uploader,
+    plugins,
+    readyStatus,
+    setReadyStatus,
+    ...fileSelectorProps
 }) => {
-  const [files, setFiles] = useState<string[]>([]);
-  const { register, setValue, formState } = useFormContext();
+    const [files, setFiles] = useState<string[]>([]);
+    const { register, setValue, formState } = useFormContext();
 
-  useEffect(() => {
-    const handleFilesAdded = (uppy_files: { data: { name: string } }[]) => {
-      setFiles((prevFiles) => [...prevFiles, ...uppy_files.map((a) => a.data.name)]);
-    };
+    useEffect(() => {
+        const handleFilesAdded = (uppy_files: { data: { name: string } }[]) => {
+            setFiles((prevFiles) => [...prevFiles, ...uppy_files.map((a) => a.data.name)]);
+        };
 
-    const handleFileRemoved = (file: { name: string }, reason: string) => {
-      setFiles((prevFiles) => {
-        if (reason === "cancel-all" || !prevFiles.length) {
-          return [];
+        const handleFileRemoved = (file: { name: string }, reason: string) => {
+            setFiles((prevFiles) => {
+                if (reason === "cancel-all" || !prevFiles.length) {
+                    return [];
+                }
+                return prevFiles.filter((e) => e !== file.name);
+            });
+        };
+
+        const handleCancelAll = () => {
+            setFiles([]);
+            setReadyStatus?.((prevState) => ({ ...prevState, [name]: null }));
+        };
+
+        const handleError = (error: Error) => {
+            console.log(error);
+            setFiles([]);
+        };
+
+        const handleComplete = (result: { successful: any[] }) => {
+            setReadyStatus?.((prevState) => ({
+                ...prevState,
+                [name]: result.successful.length > 0 ? true : false,
+            }));
+            setFiles([]);
+        };
+
+        uploader.on("files-added", handleFilesAdded);
+        uploader.on("file-removed", handleFileRemoved);
+        uploader.on("cancel-all", handleCancelAll);
+        uploader.on("error", handleError);
+        uploader.on("complete", handleComplete);
+
+        return () => {
+            uploader.off("files-added", handleFilesAdded);
+            uploader.off("file-removed", handleFileRemoved);
+            uploader.off("cancel-all", handleCancelAll);
+            uploader.off("error", handleError);
+            uploader.off("complete", handleComplete);
+        };
+    }, [files, readyStatus, name]);
+
+    useEffect(() => {
+        register(name, { required: true });
+    }, [name, register]);
+
+    useEffect(() => {
+        if (files.length === 0) {
+            setValue(name, null);
+        } else {
+            setValue(name, files, { shouldValidate: true });
         }
-        return prevFiles.filter((e) => e !== file.name);
-      });
-    };
+    }, [files, name, setValue]);
 
-    const handleCancelAll = () => {
-      setFiles([]);
-      setReadyStatus?.((prevState) => ({ ...prevState, [name]: null }));
-    };
-
-    const handleError = (error: Error) => {
-      console.log(error);
-      setFiles([]);
-    };
-
-    const handleComplete = (result: { successful: any[] }) => {
-      setReadyStatus?.((prevState) => ({
-        ...prevState,
-        [name]: result.successful.length > 0 ? true : false,
-      }));
-      setFiles([]);
-    };
-
-    uploader.on("files-added", handleFilesAdded);
-    uploader.on("file-removed", handleFileRemoved);
-    uploader.on("cancel-all", handleCancelAll);
-    uploader.on("error", handleError);
-    uploader.on("complete", handleComplete);
-
-    return () => {
-      uploader.off("files-added", handleFilesAdded);
-      uploader.off("file-removed", handleFileRemoved);
-      uploader.off("cancel-all", handleCancelAll);
-      uploader.off("error", handleError);
-      uploader.off("complete", handleComplete);
-    };
-  }, [files, readyStatus, name]);
-
-  useEffect(() => {
-    register(name, { required: true });
-  }, [name, register]);
-
-  useEffect(() => {
-    if (files.length === 0) {
-      setValue(name, null);
-    } else {
-      setValue(name, files, { shouldValidate: true });
-    }
-  }, [files, name, setValue]);
-
-  return (
-    <>
-      <Box sx={{ border: formState.errors[name] ? '2px solid red' : undefined }}>
-        <FileSelector
-          name={name}
-          uploader={uploader}
-          plugins={plugins}
-          {...fileSelectorProps}
-        />
-      </Box>
-      {formState.errors[name] && (
-        <FormHelperText error>
-          {formState.errors[name]?.message}
-        </FormHelperText>
-      )}
-    </>
-  );
+    return (
+        <>
+            <Box sx={{ border: formState.errors[name] ? '2px solid red' : undefined }}>
+                <FileSelector
+                    name={name}
+                    uploader={uploader}
+                    plugins={plugins}
+                    {...fileSelectorProps}
+                />
+            </Box>
+            {formState.errors[name] && (
+                <FormHelperText error>
+                    {(formState.errors[name]?.message as string) || null}
+                </FormHelperText>
+            )}
+        </>
+    );
 };
 
 export default FileSelectorField;
